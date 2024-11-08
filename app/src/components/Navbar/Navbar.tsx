aec623bd5cda1c550c206a9181bc92556acb3966
Muhammad Hassan <mhassan.49354@gmail.com>
2024-11-08 10:18:04 +0500

    resolve width issue

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar({ name }: { name: string }) {
 
   return (
     <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
